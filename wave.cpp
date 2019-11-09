ca1c6f1ad87f33af7cd041882692989c2d9eb755
Gilaverbuch <46658204+Gilaverbuch@users.noreply.github.com>
2019-11-09 20:03:15 +0100

    wave.cpp model parameters class

--- wave.cpp
+++ wave.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 #include <cmath>
 
+class model_parameters{
+public:
+	int l, nsteps, dx, elements, density, velocity;
+	double dt; 
+	model_parameters(int l_, int nstep_, int dx_, int elements_, int density_, int velocity_, double dt_);
+
+private:
+
+
+};
+
+model_parameters::model_parameters(int l_, int nstep_, int dx_, int elements_, int density_, int velocity_, double dt_){
+	l = l_;
+	nsteps = nstep_;  
+	dx = dx_; 
+	elements = elements_; 
+	density = density_; 
+	velocity = velocity_;
+	dt = dt_;
+}
+
+
 
 int main()
 {	
-	// --------------------------------------------------------------
+	// --------------------------------------------------------------------------------------------
 	// define model parameters --> put in class with constructors 
 
-	int l = 10000; //length [m]
-	int nsteps = 2000; //number of timesteps
-	int dx = 20; //[m]
-	double dt = 0.005; //[sec]
-	int elements = int(l/dx);
-	int density = 2800; //[kg/m^3]
-	int velocity = 3000; //[m/s]
+	int l_ = 10000; //length [m]
+	int nsteps_ = 2000; //number of timesteps
+	int dx_ = 20; //[m]
+	int elements_ = int(l_/dx_);
+	int density_ = 2800; //[kg/m^3]
+	int velocity_ = 3000; //[m/s]
+	double dt_ = 0.005; //[sec]
 
-	// --------------------------------------------------------------
-	std::cout << "number of elements" << " " << elements << std::endl;
+	// --------------------------------------------------------------------------------------------
+
+
+
+	model_parameters model(l_, nsteps_, dx_, elements_, density_, velocity_, dt_);;
+	std::cout << "model parameters" << " " << model.nsteps << std::endl;
+
+	std::cout << "number of elements" << " " << elements_ << std::endl;
 	int n=10;
 	int a[n] = {};
 
