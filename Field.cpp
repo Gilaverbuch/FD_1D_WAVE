c7855940b1fd8f09a6b796a30cc789e4d5999290
Gilaverbuch <46658204+Gilaverbuch@users.noreply.github.com>
2019-11-10 10:33:19 +0100

    adding Field class and header

---
+++ Field.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <sstream> 
+#include "Field.h"
+
+Field::Field(int size){
+
+	std::cout << "Cuctum default constructor. Initializing the field parameters" << std::endl;
+
+	U = new double [size];
+	U_past = new double [size];
+	U_future = new double [size]; 
+	lambda = new double [size];
+	rho = new double [size];
+	vel = new double [size];
+	epsilon = new double [size];
+	RHS = new double [size];
+	sig = new double [size];
+	x = new double [size];
+}
---
+++ Field.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+class Field{
+public:
+	double *U, *U_past, *U_future, *lambda, *rho, *vel, *epsilon, *RHS, *sig, *x;
+	Field(int size);	// Custom default constructor
+	
+private:
+	
+
+};
