4d703eb108ed13a3f73feddf47e23e08b6ed3685
Gilaverbuch <46658204+Gilaverbuch@users.noreply.github.com>
2019-11-10 10:35:53 +0100

    edit

--- makefile
+++ makefile
@@ -1,10 +1,13 @@
 all: main
 
-main: wave.o model_parameters.o
-		 g++ -o wave wave.o model_parameters.o
+main: wave.o model_parameters.o Field.o
+		 g++ -o wave wave.o model_parameters.o Field.o
 
 model_parameters.o: model_parameters.cpp
 		g++ -c model_parameters.cpp
+
+Field.o: Field.cpp
+		g++ -c Field.cpp
 
 wave.o: wave.cpp
 		g++ -c wave.cpp 
--- wave.cpp
+++ wave.cpp
@@ -3,35 +3,7 @@
 #include <fstream>
 #include <sstream> 
 #include "model_parameters.h"
-
-
-// --------------------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------------
-class Field{
-public:
-	double *U, *U_past, *U_future, *lambda, *rho, *vel, *epsilon, *RHS, *sig, *x;
-	Field(int size);	// Custom default constructor
-	
-private:
-	
-
-};
-
-Field::Field(int size){
-
-	std::cout << "Cuctum default constructor. Initializing the field parameters" << std::endl;
-
-	U = new double [size];
-	U_past = new double [size];
-	U_future = new double [size]; 
-	lambda = new double [size];
-	rho = new double [size];
-	vel = new double [size];
-	epsilon = new double [size];
-	RHS = new double [size];
-	sig = new double [size];
-	x = new double [size];
-}
+#include "Field.h"
 
 
 // --------------------------------------------------------------------------------------------
@@ -43,14 +15,6 @@ int main()
 	model.print_parameters();
 	Field displacement(model.elements);
 
-	// this is another way to print the model parameters
-	// std::cout << "length" << " " << model.l << std::endl;
-	// std::cout << "dx" << " " << model.dx << std::endl;
-	// std::cout << "number of elements" << " " << model.elements << std::endl;
-	// std::cout << "dt" << " " << model.dt << std::endl;
-	// std::cout << "number of stepss" << " " << model.nsteps << std::endl;
-	// std::cout << "density" << " " << model.density << std::endl;
-	// std::cout << "velocity" << " " << model.velocity << std::endl;
 
 
 	// std::cout << "number of elements" << " " << elements_ << std::endl;
